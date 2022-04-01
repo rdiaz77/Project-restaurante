e3a1103657db256a93d5969ba98a00db1cfbd27b
RDM_ <rafadiazm77@gmail.com>
2022-04-01 16:27:21 -0500

    route tables to README

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) =>{
-    res.send('GET/places')
-
+router.get('/', (req, res) => {
+    res.send('GET /places ctm')
 })
+
 module.exports = router
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
-const express = require('express') //this call express to the module
-const app = express () // this instantiate express under app
+const express = require('express')
+const app = express()
 
 
-app.get ('/', (req,res) => {
-    res.send("hello world")
+
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
 })
 
-app.get('*', (req, res) =>{
+app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
