bf8e7ac392b58352412ae42dec718e192d4f9adf
RDM_ <rafadiazm77@gmail.com>
2022-04-03 10:40:11 -0500

    form data getting through

--- controllers/places.js
+++ controllers/places.js
@@ -20,6 +20,18 @@ router.get('/', (req, res) => {
 router.get('/new', (req,res) => {
   res.render('places/new')
 
-
 })
+ router.post('/', (req,res) =>{
+   console.log(req.body)
+   res.send('POST/Places')
+ })
+
+
+
+
+
+
+
+
+
 module.exports = router
