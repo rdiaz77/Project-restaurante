1c3dbc4358fdcadc6d486f717ce032a0ddbbc6da
RDM_ <rafadiazm77@gmail.com>
2022-04-03 12:42:42 -0500

    delete fx implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,33 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <div>
+                <img src= {data.place.pic} alt="photo of food" />
+            </div>
+            <div>
+                <h2>Rating</h2>
+            </div>
+            <div>
+                <h2>Description</h2>
+            </div>
+            <div>
+                <h2>Comments</h2>
+            </div>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                Edit
+            </a>      
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">
+                    Delete
+                </button>
+            </form> 
+            
+
           </main>
         </Def>
     )
