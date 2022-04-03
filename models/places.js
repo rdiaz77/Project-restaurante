f55f57148008410fdc025695b72a8d61fe692655
RDM_ <rafadiazm77@gmail.com>
2022-04-03 11:14:09 -0500

    new places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'My Burger Cafe',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/burger.jpg'
+  }, {
+    name: 'Italian Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/pizza.jpg'
+  }]
